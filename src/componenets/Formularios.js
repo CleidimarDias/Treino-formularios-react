200ebdf47bd1c66de8e827206ac3e3b952fb8697
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-02-03 17:04:18 -0300

    feat: adicionado select estado

--- src/componenets/Formularios.js
+++ src/componenets/Formularios.js
@@ -5,13 +5,17 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 import * as yup from 'yup';
 
 let schema = yup.object().shape({
   name: yup.string().required('O nome é obrigatório'),
   fone: yup.string().required('O telefone é obrigatório').matches(/^\d+$/, 'O telefone é inválido'),
   email: yup.string().email('E-mail inválido').required('O E-mail é obrigatório'),
-  cpf: yup.string().required('O CPF é obrigatório')
+  cpf: yup.string().required('O CPF é obrigatório'),
+  estado: yup.string().required('Estado é obrigatório'),
 });
 
 
@@ -21,14 +25,15 @@ function App() {
   const [fone, setFone] = useState('');
   const [email, setEmail] = useState('');
   const [cpf, setCpf] = useState('');
+  const [estado, setEstado] =useState('');
   const [errors, setErrors] = useState({});
-  
+
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
     
     try {
-      await schema.validate({name, fone, email, cpf}, {abortEarly: false});
+      await schema.validate({name, fone, email, cpf, estado}, {abortEarly: false});
       console.log('formulário válido');
       setErrors({});
 
@@ -36,6 +41,7 @@ function App() {
       console.log(fone);
       console.log(email);
       console.log(cpf);
+      console.log(estado);
       
 
     }catch (errors){
@@ -56,7 +62,7 @@ function App() {
       component="form" onSubmit={handleSubmit}
       backgroundColor='yellow'
       sx={{
-        '& > :not(style)': { m: 2, width: '40ch' },        
+        '& > :not(style)': { m: 1, width: '40ch' },        
         
       }}
       noValidate
@@ -77,6 +83,23 @@ function App() {
        {errors.cpf && <p>{errors.cpf}</p>}
        <TextField required fullWidth id="outlined-basic" label="CPF"
        variant="outlined"  onChange={(e)=>setCpf(e.target.value)} />
+
+<Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="select-label">Estado</InputLabel>
+        <Select
+          labelId="select-label-id"
+          id="simple-select"
+          value={estado}
+          label="Estado"
+          onChange={(e)=>setEstado(e.target.value)}
+        >
+          <MenuItem value={'go'}>Goiás</MenuItem>
+          <MenuItem value={'df'}>Distrito</MenuItem>
+          <MenuItem value={'sp'}>São Paulo</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
        
 
         <Stack direction="row" spacing={2}> 
